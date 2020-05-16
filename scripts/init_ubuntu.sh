0eb59f515c9b9c6097855ab3ca9307b10b0a2058
g1212 <ghill201@caledonian.ac.uk>
2020-05-16 16:37:02 +0100

    Added terraform

--- scripts/init_ubuntu.sh
+++ scripts/init_ubuntu.sh
@@ -4,11 +4,11 @@ sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install python-virtualenv python3-pip -y
 
-mkdir /home/vagrant/ansible2.9
+mkdir /home/vagrant/.ansible2.9
 
-virtualenv /home/vagrant/ansible2.9
+virtualenv /home/vagrant/.ansible2.9
 
-source /home/vagrant/ansible2.9/bin/activate
+source /home/vagrant/.ansible2.9/bin/activate
 
 pip3 install ansible==2.9
 
